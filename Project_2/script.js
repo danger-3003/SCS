1928d1eff3715bd05123ad01cafb0f137dbd693c
danger-3003 <naremsumanth@gmail.com>
2023-06-24 16:37:09 +0530

    project 2

--- Project_2/script.js
+++ Project_2/script.js
@@ -11,3 +11,25 @@ toggle.addEventListener("click",()=>{
         toggle.classList.replace("fa-times","fa-bars");
     }
 })
+function menu_card()
+{
+    let menu_cards=document.getElementById("menu_cards");
+    let menu_card1=document.querySelector(".menu_card1");
+    let menu_card2=document.querySelector(".menu_card2");
+    let view_more = document.getElementById("view_more");
+    menu_cards.classList.toggle("p-10");
+    console.log(menu_cards);
+    menu_cards.classList.toggle("h-auto");
+    if (view_more.innerText === "View More")
+    {
+        view_more.innerText="View Less";
+        menu_card1.classList.replace("h-0","h-72");
+        menu_card2.classList.replace("h-0","h-72");
+    }
+    else
+    {
+        view_more.innerText="View More";
+        menu_card1.classList.replace("h-72","h-0");
+        menu_card2.classList.replace("h-72","h-0");
+    }
+}
